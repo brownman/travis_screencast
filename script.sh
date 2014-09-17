35ea018844285c87c69fd7442594d776159570bc
brownman <brownman556@gmail.com>
2014-09-17 09:46:37 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
+owner=brownman
+repo=travis_test
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
@@ -6,7 +9,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Uko/Rubidium-WHOIS.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$owner/$repo.git  gh-pages > /dev/null
 
   cd gh-pages
   cp -Rf $HOME/coverage/* .
