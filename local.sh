5e96c1f3249a95f362a3fd345510a6dd690aa505
brownman <brownman556@gmail.com>
2014-09-17 14:15:56 +0300

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,3 +1,3 @@
-source /tmp/library.cfg
-use commander
-commander travis encrypt GH_TOKEN=$TOKEN --add
+gem install travis
+echo now go get the OAUTH TOKEN
+travis encrypt GH_TOKEN=$TOKEN --add env.global
