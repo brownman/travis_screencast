d538c9ef0a250bbd452b6cbf45b94c5fdd3dcbb7
brownman <brownman556@gmail.com>
2014-09-17 14:39:00 +0300

    add missing dependencies

--- SCRIPTS/prototypes.sh
+++ SCRIPTS/prototypes.sh
@@ -1,5 +1,5 @@
 
-sudo apt-get install -y fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
+sudo apt-get install -y flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
 sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
 sudo apt-get update 
 sudo apt-get install yad
