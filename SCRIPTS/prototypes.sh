48c23ea23f2a8b6e8823d8a2ce1338da63971382
brownman <brownman556@gmail.com>
2014-09-17 15:35:47 +0300

    try to record what's happend

--- SCRIPTS/prototypes.sh
+++ SCRIPTS/prototypes.sh
@@ -2,11 +2,12 @@ flite_test(){
 #alias flite=`which flite`
 str=$(dirname `which flite`)
 export PATH=$PATH:$str  
+flite -t hello -o /tmp/1.wav
+paplay <(cat /tmp/1.wav)
 }
-sudo apt-get install flite
-flite_test 
-flite -t hello
-exit
+
+
+
 
 sudo apt-get install -y flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
 sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
@@ -15,8 +16,33 @@ sudo apt-get install yad
 
 
 
+run(){
+flite_test   
 cd /tmp
 git clone https://github.com/brownman/prototypes
 cd prototypes
 ./LIB/install.sh
 ./BANK/sanity_loop/run.sh
+}
+
+
+
+eval "export DISPLAY=:99.0"
+eval "sh -e /etc/init.d/xvfb start"
+dbus-launch pulseaudio --start
+eval "recordmydesktop --no-cursor  --output=session.ogv &"
+paplay <(espeak -vmb-us2  "Ok, let's get rickrolled" --stdout)
+#eval "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
+run
+sleep 10
+paplay <(espeak -vmb-us2  "That bored me" --stdout)
+killall firefox 
+sleep 2
+paplay <(espeak -vmb-us2  "Much better now" --stdout)
+sleep 2
+paplay <(espeak -vmb-us2  "I have to go. See you next build!" --stdout)
+killall recordmydesktop
+while pgrep -x recordmydesktop > /dev/null ; do sleep 1; done # wait for transcoding
+tar cf artifacts.tar session.ogv
+ls -l session.ogv
+mv session.ogv /tmp
