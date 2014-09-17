e1c6fdc89e22116a5688462235dd05e141220689
brownman <brownman556@gmail.com>
2014-09-17 10:49:05 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,14 +29,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$owner/$repo.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$owner/$repo.git  gh-pages #> /dev/null
 
-  #cd gh-pages
-  #cp -Rf $HOME/coverage/* .
+  cd gh-pages
+  cp -Rf $HOME/coverage/* .
 
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages #> /dev/null
 
   echo -e "Done magic with coverage\n"
 fi
