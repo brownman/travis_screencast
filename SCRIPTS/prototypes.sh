9c690181d93b4b7af70983e7939b4c66ff8317bf
brownman <brownman556@gmail.com>
2014-09-17 14:45:57 +0300

    flite not found - alias OR  fix PATH ?
    lets check alias firs

--- SCRIPTS/prototypes.sh
+++ SCRIPTS/prototypes.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
 sudo apt-get update 
 sudo apt-get install yad
 
-
+alias flite=`which flite`
 
 cd /tmp
 git clone https://github.com/brownman/prototypes
