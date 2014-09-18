95f9278b710f9fe4820287e14157a65807bd0186
brownman <brownman556@gmail.com>
2014-09-18 14:42:31 +0300

    Update record_start.sh

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -1,32 +1,8 @@
-
-
-steps(){
-local pid
-eval "export DISPLAY=:99.0"
-eval "sh -e /etc/init.d/xvfb start"
-dbus-launch pulseaudio --start
-
-eval "recordmydesktop --no-cursor  --output=session.ogv &"
-paplay <(espeak -vmb-us2  "Ok, let's get rickrolled" --stdout)
-#eval "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
-
-#start pid, delay, kill
-( TMOUT=20;   commander $router script  )
-
-
-sleep 10
-paplay <(espeak -vmb-us2  "That bored me" --stdout)
-
-
-sleep 2
-paplay <(espeak -vmb-us2  "Much better now" --stdout)
-sleep 2
-paplay <(espeak -vmb-us2  "I have to go. See you next build!" --stdout)
-killall recordmydesktop
-while pgrep -x recordmydesktop > /dev/null ; do sleep 1; done # wait for transcoding
-tar cf artifacts.tar session.ogv
-ls -l session.ogv
-mv session.ogv /tmp
-}
-steps
  
+commander "export DISPLAY=:99.0"
+commander "sh -e /etc/init.d/xvfb start"
+commander dbus-launch pulseaudio --start
+
+commander  "recordmydesktop --no-cursor  --output=session.ogv &"
+
+
