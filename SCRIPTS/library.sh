48cbc5b7ca6826bd1ca13d48228ce6539cc60caa
brownman <brownman556@gmail.com>
2014-09-18 12:19:44 +0300

    Update library.sh

--- SCRIPTS/library.sh
+++ SCRIPTS/library.sh
@@ -2,7 +2,7 @@
 
  commander cd $HOME
   commander git clone https://github.com/brownman/LIBRARY --depth=1
-  commander source $HOME/LIBRARY/install.cfg
+  commander source $HOME/LIBRARY/library.cfg
    
   commander source /tmp/library.cfg
   commander use1
