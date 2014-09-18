648082ee82f81f20ef6f168b55b3e544c89295dc
brownman <brownman556@gmail.com>
2014-09-18 12:33:00 +0300

    Update library.sh

--- SCRIPTS/library.sh
+++ SCRIPTS/library.sh
@@ -6,6 +6,8 @@ steps_for_library(){
  commander cd $HOME
   commander git clone https://github.com/brownman/LIBRARY --depth=1
   dir2=$HOME/LIBRARY/library.cfg
+  tree $dir2
+  
   commander source $dir2/library.cfg
  
   commander source /tmp/library.cfg
