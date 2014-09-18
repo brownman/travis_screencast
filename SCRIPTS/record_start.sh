c21c8cd24bee980cf7a31850aeb1ccb78bf91570
brownman <brownman556@gmail.com>
2014-09-18 16:22:10 +0300

    ensure dependencies are met

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -5,7 +5,10 @@ commander dbus-launch pulseaudio --start
 
 #commander  "recordmydesktop --no-cursor  --output=session.ogv &"
 #` use_sh screencast ` &
-commander byzanz-record --duration=$timeout_record --x=0 --y=0 --width=1280 --height=800 $file_product
+use assert
+util=byzanz-record
+depend $util
+commander $util --duration=$timeout_record --x=0 --y=0 --width=1280 --height=800 $file_product
 #/tmp/screencast/15_24_16_18_09_14.gif 
 
 
