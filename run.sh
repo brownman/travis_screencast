a248b8847fb9ffb870d53a38ba9faf6bcd43c46d
brownman <brownman556@gmail.com>
2014-09-18 10:11:57 +0300

    run.sh - act as a router

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
 #
+args="$@"
+
+dir1=./SCRIPTS
+file_proto=$dir/prototypes.sh
+file_github=$dir/push_to_github.sh
+
+$file_proto $args
+
+size=$( du /tmp/session.ogv | cut -d'.'  -f1 ) 
+echo "[SIZE] $size"
+if [ $size -gt 9000000 ];then
+$file_github
+fi
