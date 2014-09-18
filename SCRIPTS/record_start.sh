f395295ea12f7a155fdd174280b693dc48332199
brownman <brownman556@gmail.com>
2014-09-18 15:27:43 +0300

    recorder: replace script with a short commnd

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -4,6 +4,9 @@ commander "sh -e /etc/init.d/xvfb start"
 commander dbus-launch pulseaudio --start
 
 #commander  "recordmydesktop --no-cursor  --output=session.ogv &"
-` use_sh screencast ` &
+#` use_sh screencast ` &
+byzanz-record --duration=60 --x=0 --y=0 --width=1280 --height=800 $file_product
+#/tmp/screencast/15_24_16_18_09_14.gif 
+
 
 
