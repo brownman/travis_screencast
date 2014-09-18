e2cc68bb55f542ccf2a5d629a96ca6166e10ecc0
brownman <brownman556@gmail.com>
2014-09-18 15:49:08 +0300

    Update record_start.sh

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -5,7 +5,7 @@ commander dbus-launch pulseaudio --start
 
 #commander  "recordmydesktop --no-cursor  --output=session.ogv &"
 #` use_sh screencast ` &
-byzanz-record --duration=60 --x=0 --y=0 --width=1280 --height=800 $file_product
+commander byzanz-record --duration=60 --x=0 --y=0 --width=1280 --height=800 $file_product
 #/tmp/screencast/15_24_16_18_09_14.gif 
 
 
