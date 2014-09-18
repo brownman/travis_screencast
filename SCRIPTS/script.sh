9faba55c2af5afa345f08067bd16c11672f56dd0
brownman <brownman556@gmail.com>
2014-09-18 13:10:27 +0300

    add test for gui tools

--- SCRIPTS/script.sh
+++ SCRIPTS/script.sh
@@ -6,14 +6,19 @@ flite -t hello -o /tmp/1.wav
 paplay <(cat /tmp/1.wav)
 }
 
-test_repo1(){
+test_prototype(){
 cd /tmp
 git clone https://github.com/brownman/prototypes
 cd prototypes
 ./LIB/install.sh
 ./BANK/sanity_loop/run.sh
 }
-
+text_simple_gui(){
+  xcowsay hi
+  notify-send hello world
+  gxmessage hi -t 3
+  yad hi --timeout=3
+}
 
 test -f $file_list_session || exiting
 while read line;do
