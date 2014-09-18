a1914fff213bbbf4df193944fd2144e1d805f940
brownman <brownman556@gmail.com>
2014-09-18 13:47:46 +0300

    don't use alias, use real func name

--- SCRIPTS/library.sh
+++ SCRIPTS/library.sh
@@ -2,19 +2,19 @@
 
 
 steps_for_library(){
- commander cd $HOME
+ commander1 cd $HOME
  local url_repo=https://github.com/brownman/LIBRARY
-  commander git clone  --depth=1 $url_repo --branch=develop
+  commander1 git clone  --depth=1 $url_repo --branch=develop
   #git archive --format=tar --remote=$url_repo HEAD | tar xf -
   local dir2
   dir2=$HOME/LIBRARY
   
   #commander tree $dir2
   
-  commander source $dir2/library.cfg
+  commander1 source $dir2/library.cfg
  
-  commander source /tmp/library.cfg
-  commander use1
-  commander print ok 
+  commander1 source /tmp/library.cfg
+  commander1 use1
+  commander1 print ok 
 }
 steps_for_library
