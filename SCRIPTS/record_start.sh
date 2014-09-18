ae2f7b272328cc5c8e9c57957810e930d172c445
brownman <brownman556@gmail.com>
2014-09-18 14:59:19 +0300

    replace recorder

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -3,6 +3,7 @@ commander "export DISPLAY=:99.0"
 commander "sh -e /etc/init.d/xvfb start"
 commander dbus-launch pulseaudio --start
 
-commander  "recordmydesktop --no-cursor  --output=session.ogv &"
+#commander  "recordmydesktop --no-cursor  --output=session.ogv &"
+` use_sh screencast `
 
 
