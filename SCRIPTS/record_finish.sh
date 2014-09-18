1542c5372e92d3adf96896f37cddecb1094d9c2a
brownman <brownman556@gmail.com>
2014-09-18 15:51:37 +0300

    much cleaner..

--- SCRIPTS/record_finish.sh
+++ SCRIPTS/record_finish.sh
@@ -1,5 +1,6 @@
-killall recordmydesktop
-while pgrep -x recordmydesktop > /dev/null ; do sleep 1; done # wait for transcoding
-tar cf artifacts.tar session.ogv
-ls -l session.ogv
-mv session.ogv /tmp
+#echo killall recordmydesktop
+#while pgrep -x recordmydesktop > /dev/null ; do sleep 1; done # wait for transcoding
+#tar cf artifacts.tar $file_product
+ls -l $file_product
+#session.ogv
+
