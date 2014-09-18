0bf286be5b435a199f9ad652e7974f8255b1c004
brownman <brownman556@gmail.com>
2014-09-18 16:40:47 +0300

    add 1 PPA

--- SCRIPTS/install_deps.sh
+++ SCRIPTS/install_deps.sh
@@ -1,9 +1,11 @@
 
 sudo apt-get update -qq
 sudo apt-get install -qq recordmydesktop x11-apps libpulse-dev pulseaudio dbus-x11 espeak mbrola-us2 pulseaudio-utils
+sudo apt-get install -y  flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
 
-sudo apt-get install -y byzanz-record flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
+#PPA
 sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
+sudo add-apt-repository ppa:fossfreedom/byzanz -y 
 sudo apt-get update 
-sudo apt-get install yad
+sudo apt-get install yad byzanz-record
 
