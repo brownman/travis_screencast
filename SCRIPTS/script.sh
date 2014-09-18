354f8d22775bce616ee8e5f4d392ba0f33164ec1
brownman <brownman556@gmail.com>
2014-09-18 10:23:03 +0300

    contain many tests
    caller line: will be wrapped by the recorder script

--- SCRIPTS/script.sh
+++ SCRIPTS/script.sh
@@ -1,7 +1,18 @@
+test_flite(){
+#alias flite=`which flite`
+str=$(dirname `which flite`)
+export PATH=$PATH:$str  
+flite -t hello -o /tmp/1.wav
+paplay <(cat /tmp/1.wav)
+}
 
-
+test_repo1(){
 cd /tmp
 git clone https://github.com/brownman/prototypes
 cd prototypes
 ./LIB/install.sh
 ./BANK/sanity_loop/run.sh
+}
+
+args="$@"
+$args
