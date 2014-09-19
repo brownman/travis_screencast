90cf5f3b9cfadb1f145f5c94eb5eaea6b516118e
brownman <brownman556@gmail.com>
2014-09-19 15:06:45 +0300

    debug screencast recorder

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -3,14 +3,16 @@ commander "export DISPLAY=:99.0"
 commander "sh -e /etc/init.d/xvfb start"
 commander dbus-launch pulseaudio --start
 
-commander xvfb -screen 0 1024x768x24
+util_x11=$(which xvfb)
+commander $util_x11 -screen 0 1024x768x24
 
 #commander  "recordmydesktop --no-cursor  --output=session.ogv &"
 #` use_sh screencast ` &
 use assert
-util=byzanz-record
+util=
 depend $util
-commander $util --duration=$timeout_record --x=0 --y=0 --width=1280 --height=800 $file_product 
+commander $util -v --duration=$timeout_record --x=0 --y=0 --width=1280 --height=800 $file_product --cursor
+#--cursor=NULL
 #--display=$DISPLAY
 #/tmp/screencast/15_24_16_18_09_14.gif 
 
