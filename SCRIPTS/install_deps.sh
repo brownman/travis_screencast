c37b0df56a204dae48bbda0be25c610078f97f97
brownman <brownman556@gmail.com>
2014-09-19 15:09:02 +0300

    add dependency for utility: notify-send

--- SCRIPTS/install_deps.sh
+++ SCRIPTS/install_deps.sh
@@ -1,7 +1,7 @@
 
 sudo apt-get update -qq
 sudo apt-get install -qq recordmydesktop x11-apps libpulse-dev pulseaudio dbus-x11 espeak mbrola-us2 pulseaudio-utils
-sudo apt-get install -y  flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
+sudo apt-get install -y  libnotify-bin flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
 sudo apt-get install xvfb
 sudo apt-get install libgtk2.0-0
 #PPA
