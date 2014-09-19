d3e5afd7bb816289ee49eee1eca6b454838fea59
brownman <brownman556@gmail.com>
2014-09-19 15:22:08 +0300

    temporary: record using recordMydesktop

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -5,13 +5,28 @@ commander dbus-launch pulseaudio --start
 
 util_x11=$(which xvfb)
 commander $util_x11 -screen 0 1024x768x24
-
+#https://wiki.ubuntu.com/ScreenCasts/RecordMyDesktop
+#commander recordmydesktop --width 640 --height 480 -x 200 -y 200 --full-shots --fps 15   --channels 1 --device pulse --v_quality 63 --s_quality 10 --v_bitrate 2000000   --delay 10
 #commander  "recordmydesktop --no-cursor  --output=session.ogv &"
-#` use_sh screencast ` &
+options="--width 640 --height 480  \ 
+--full-shots --fps 15  \
+--channels 1 \
+--device pulse \
+--v_quality 63 \
+--s_quality 10 \
+--v_bitrate 2000000 \
+--delay $timeout_record \
+--outoput=$file_product"
+
+commander recordmydesktop $options
+
+
+record1(){
 use assert
 util=byzanz-record
 depend $util
 commander $util -v --duration=$timeout_record --x=0 --y=0 --width=1280 --height=800 $file_product --cursor
+}
 #--cursor=NULL
 #--display=$DISPLAY
 #/tmp/screencast/15_24_16_18_09_14.gif 
