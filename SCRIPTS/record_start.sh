29844e0e6737c3a707461a5a07ede592b811df3b
brownman <brownman556@gmail.com>
2014-09-19 15:32:42 +0300

    mv code to funcs

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -1,13 +1,13 @@
- 
-commander "export DISPLAY=:99.0"
-commander "sh -e /etc/init.d/xvfb start"
-commander dbus-launch pulseaudio --start
 
-util_x11=$(which xvfb)
-commander $util_x11 -screen 0 1024x768x24
+
+#util_x11=$(which xvfb)
+#commander $util_x11 -screen 0 1024x768x24
+
+
 #https://wiki.ubuntu.com/ScreenCasts/RecordMyDesktop
 #commander recordmydesktop --width 640 --height 480 -x 200 -y 200 --full-shots --fps 15   --channels 1 --device pulse --v_quality 63 --s_quality 10 --v_bitrate 2000000   --delay 10
 #commander  "recordmydesktop --no-cursor  --output=session.ogv &"
+record0(){
 options="--width 640 --height 480  \ 
 --full-shots --fps 15  \
 --channels 1 \
@@ -19,6 +19,7 @@ options="--width 640 --height 480  \
 --outoput=$file_product"
 
 commander recordmydesktop $options
+}
 
 
 record1(){
@@ -33,3 +34,4 @@ commander $util -v --duration=$timeout_record --x=0 --y=0 --width=1280 --height=
 
 
 
+record0
