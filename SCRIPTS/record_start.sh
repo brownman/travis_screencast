485eb5d5357410b07a2400818c37fbe6fa7825bb
brownman <brownman556@gmail.com>
2014-09-19 15:10:46 +0300

    Update record_start.sh

--- SCRIPTS/record_start.sh
+++ SCRIPTS/record_start.sh
@@ -9,7 +9,7 @@ commander $util_x11 -screen 0 1024x768x24
 #commander  "recordmydesktop --no-cursor  --output=session.ogv &"
 #` use_sh screencast ` &
 use assert
-util=
+util=byzanz-record
 depend $util
 commander $util -v --duration=$timeout_record --x=0 --y=0 --width=1280 --height=800 $file_product --cursor
 #--cursor=NULL
