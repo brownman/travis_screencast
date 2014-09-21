944fbf311fd2ea19fcfbbf7af9ee94fc59d3bc22
brownman <brownman556@gmail.com>
2014-09-21 13:43:13 +0300

    switch between windows
    use xdotool , wmctrl

--- SCRIPTS/tests.sh
+++ SCRIPTS/tests.sh
@@ -22,6 +22,8 @@ test_simple_gui(){
 test_firefox(){
 # commander  sudo apt-get install firefox -y
 commander "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
+wmctrl -a firefox
+xdotool key space
 commander sleep 10
 paplay <(espeak -vmb-us2  "That bored me" --stdout)
 killall firefox  
