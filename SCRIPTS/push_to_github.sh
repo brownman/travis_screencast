4fbeb36680d0a7cdaf8ba1dba302a55afa5f5f24
brownman <brownman556@gmail.com>
2014-09-21 16:24:01 +0300

    Update push_to_github.sh

--- SCRIPTS/push_to_github.sh
+++ SCRIPTS/push_to_github.sh
@@ -5,49 +5,27 @@ set -u
 
 
 upload(){
- 
-
 local path=/tmp/1
-#`where_am_i $0`
-local dirname=coverage
-local dir=$path/$dirname
-mkdir -p $dir
-
-filename=date.txt
+local dir=$dir_product
+local file_info=$dir/date.txt
 
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-  
-  
-file=$dir/$filename
-
-echo "$( date ) "  >> $file
-
-echo [ file info/location ] 
-ls -l $file
-cat $file
-mv $dir_product/* $dir/
-#mv $file_product_cover $dir/
-  
+echo "ls -l $dir_product"  > $file_info
   
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
-  
-  cp -R $dir $HOME/coverage
   cd $HOME
-  
-
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$owner/$repo.git  gh-pages #> /dev/null
-
   cd gh-pages
-  cp -Rf $HOME/coverage/* .
-
+  
+  cp -Rf $dir_product/* .
+  
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages #> /dev/null
-
   echo -e "Done magic with coverage\n"
 fi
 
