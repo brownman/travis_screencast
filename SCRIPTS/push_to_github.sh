6a0a33246f4dc69dd5b5433f8b7902b65287f880
brownman <brownman556@gmail.com>
2014-09-22 20:16:15 +0300

    Update push_to_github.sh

--- SCRIPTS/push_to_github.sh
+++ SCRIPTS/push_to_github.sh
@@ -30,7 +30,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   ### summary
   ls -l --sort=size log > log.txt #_${TRAVIS_BUILD_NUMBER}.txt
   ls -l --sort=size files  > files.txt #_${TRAVIS_BUILD_NUMBER}.txt
-  ln -l ./web/index.html .
+  #ln -l ./web/index.html .
  
 
   git add -f .
