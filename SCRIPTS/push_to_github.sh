bb4da8ae713e78fb0a27b2fab015e301819181b4
brownman <brownman556@gmail.com>
2014-09-22 12:47:16 +0300

    git: rm folders , override files on repo's root

--- SCRIPTS/push_to_github.sh
+++ SCRIPTS/push_to_github.sh
@@ -5,13 +5,9 @@ set -u
 
 
 upload(){
-local path=/tmp/1
-local dir=$dir_product
-local file_info=$dir_product/date.txt
 
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-echo "$(ls -l $dir_product)"  > $file_info
   
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
@@ -20,12 +16,19 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$owner/$repo.git  gh-pages #> /dev/null
   cd gh-pages
-  git rm -rf .
-  mv  $dir_product/* .
-  mv /tmp/log .
-  ls -l --sort=size log > log.txt
-  #${$TRAVIS_BUILD_NUMBER}
   
+  
+  git rm -rf log
+  git rm -rf files
+  
+  mv  $dir_product/* files
+  mv /tmp/log log
+  
+  ### summary
+  ls -l --sort=size log > log_${TRAVIS_BUILD_NUMBER}.txt
+  ls -l --sort=size files  > files_${TRAVIS_BUILD_NUMBER}.txt
+ 
+
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages #> /dev/null
