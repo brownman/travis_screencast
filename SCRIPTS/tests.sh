9fc13d1f0eedfa83918c321b473a706f0cb8b1b2
brownman <brownman556@gmail.com>
2014-09-22 08:24:08 +0300

    try to remove the pop-up

--- SCRIPTS/tests.sh
+++ SCRIPTS/tests.sh
@@ -22,8 +22,8 @@ test_simple_gui(){
 test_firefox(){
 # commander  sudo apt-get install firefox -y
 commander "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
-wmctrl -l
-wmctrl -a youtube
+xcowsay "$( wmctrl -l )"
+#wmctrl -a youtube
 xdotool key space
 commander sleep 10
 paplay <(espeak -vmb-us2  "That bored me" --stdout)
