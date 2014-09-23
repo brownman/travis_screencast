23b09fb282ed55e47656c05ec1d13044c4bee4bb
brownman <brownman556@gmail.com>
2014-09-23 12:09:19 +0300

    cover page: show only build number

--- SCRIPTS/debug_screen.sh
+++ SCRIPTS/debug_screen.sh
@@ -1,6 +1,7 @@
 debug_screen(){
 commander xwininfo -root -tree
-xcowfortune -t 3 &
+#xcowfortune -t 3 &
+xcowsay -t 3  "travis: build number: $TRAVIS_BUILD_NUMBER" &
 notify-send "travis: build number" "$TRAVIS_BUILD_NUMBER"
 sleep 1 
 commander "import -window root $file_product_cover"
