a8bd7f7e0f709d563e7182e34a16c67175e2000f
brownman <brownman556@gmail.com>
2014-09-25 14:39:54 +0300

    Delete record_finish.sh

--- SCRIPTS/record_finish.sh
+++
@@ -1,6 +0,0 @@
-#echo killall recordmydesktop
-#while pgrep -x recordmydesktop > /dev/null ; do sleep 1; done # wait for transcoding
-#tar cf artifacts.tar $file_product
-ls -l $file_product
-#session.ogv
-
