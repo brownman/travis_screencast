76658d4cc93aacfa75d69e221983ab9280765b65
brownman <brownman556@gmail.com>
2014-09-27 13:28:24 +0300

    Update install.sh

--- FFMPEG/install.sh
+++ FFMPEG/install.sh
@@ -1,1 +1,110 @@
 #https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu
+#!/bin/sh
+# https://github.com/AmericanCouncils/ac-common-ansible/blob/master/files/install_ffmpeg.sh
+#https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu
+
+
+
+# custom install script for ffmpeg for use on travis
+# TODO: Use /usr/local/bin as bindir, don't install to $HOME/bin and move after
+# fail entire script on first error
+#set -e
+#rm -rf ~/ffmpeg_sources
+#rm -rf ~/ffmpeg_build
+
+
+
+
+## dependencies
+sudo apt-get -y install autoconf automake build-essential git libass-dev libgpac-dev \
+  libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev \
+  libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev
+
+export PATH=$PATH:$HOME/bin
+
+mkdir -p ~/ffmpeg_sources
+
+## yasm
+cd ~/ffmpeg_sources
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
+tar xzvf yasm-1.2.0.tar.gz
+cd yasm-1.2.0
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+make distclean
+. ~/.profile
+
+## x264
+cd ~/ffmpeg_sources
+wget http://download.videolan.org/pub/x264/snapshots/x264-snapshot-20140813-2245-stable.tar.bz2
+tar xjvf x264-snapshot-20140813-2245-stable.tar.bz2
+cd x264-snapshot*
+PATH="$PATH:$HOME/bin" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --disable-opencl
+PATH="$PATH:$HOME/bin" make
+make install
+make distclean
+
+## fdk-aac
+cd ~/ffmpeg_sources
+git clone git://github.com/mstorsjo/fdk-aac.git
+cd fdk-aac
+git checkout 2f29dd48d02d402169246e3c7f9256869817392a
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+make distclean
+
+## libmp3lame
+sudo apt-get install nasm
+cd ~/ffmpeg_sources
+wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+tar xzvf lame-3.99.5.tar.gz
+cd lame-3.99.5
+./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --disable-shared
+make
+make install
+make distclean
+
+## libopus
+cd ~/ffmpeg_sources
+wget http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
+tar xzvf opus-1.0.3.tar.gz
+cd opus-1.0.3
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+make distclean
+
+## libvpx
+cd ~/ffmpeg_sources
+git clone http://git.chromium.org/webm/libvpx.git
+cd libvpx
+git checkout d6606d1ea734bbd7b3ab3e0852cedace5e4f0cd6
+./configure --prefix="$HOME/ffmpeg_build" --disable-examples
+make
+make install
+make clean
+
+## FFMPEG!!
+cd ~/ffmpeg_sources
+git clone git://source.ffmpeg.org/ffmpeg
+cd ffmpeg
+git checkout 48cd103
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
+export PKG_CONFIG_PATH
+./configure --prefix="$HOME/ffmpeg_build" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --bindir="$HOME/bin" --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfdk-aac \
+  --enable-libmp3lame --enable-libopus --enable-libtheora --enable-libvorbis --enable-libvpx \
+  --enable-libx264 --enable-nonfree --enable-x11grab
+make
+sudo make install
+sudo make distclean
+hash -r
+sudo mv $HOME/bin/ffmpeg /usr/local/bin/ffmpeg
+
+#rm -rf ~/ffmpeg_build ~/ffmpeg_sources ~/bin/{ffmpeg,ffprobe,ffserver,vsyasm,x264,yasm,ytasm}
+
+# TODO: Delete ffmpeg_sources and ffmpeg_build after we're done
