f979382927abf7851a8956d31220a8974406772e
brownman <brownman556@gmail.com>
2014-09-27 13:12:10 +0300

    Update record.sh

--- FFMPEG/record.sh
+++ FFMPEG/record.sh
@@ -1,1 +1,50 @@
 #
+# capture fullscreen using SERVER: alsa or pulseaudio
+ffx-full-hw() { ffmpeg -f $SERVER -ac $FFX_MONO \
+	-i $FFX_HW -f x11grab -r $FFX_FPS -s $FFX_WIN_FULL -i :0.0 \
+	-acodec $FFX_AUDIO -vcodec $FFX_VIDEO \
+       	-preset $FFX_PRESET -crf $FFX_CRF -threads $FFX_THREADS \
+	-vf $FFX_SCALE \
+	-y $FFX_OUTPUT
+}
+ffx-full-pa() { ffmpeg -f $SERVER -ac $FFX_MONO \
+	-i $FFX_PULSE -f x11grab -r $FFX_FPS -s $FFX_WIN_FULL -i :0.0 \
+	-acodec $FFX_AUDIO -vcodec $FFX_VIDEO \
+       	-preset $FFX_PRESET -crf $FFX_CRF -threads $FFX_THREADS \
+	-vf $FFX_SCALE \
+	-y $FFX_OUTPUT
+}
+
+# capture single window, use mouse cursor to select window you want to record
+ffx-winselect-hw() { 
+	FFX_INFO=$(xwininfo -frame)
+
+	ffmpeg -f $SERVER -ac $FFX_MONO \
+	-i $FFX_HW -f x11grab -r $FFX_FPS \
+	-s $(echo $FFX_INFO | grep -oEe 'geometry [0-9]+x[0-9]+'\
+	| grep -oEe '[0-9]+x[0-9]+') \
+	-i :0.0+$(echo $FFX_INFO | grep -oEe 'Corners:\s+\+[0-9]+\+[0-9]+' \
+	| grep -oEe '[0-9]+\+[0-9]+' | sed -e 's/\+/,/' ) \
+	-acodec $FFX_AUDIO -vcodec $FFX_VIDEO \
+       	-preset $FFX_PRESET -crf $FFX_CRF -threads $FFX_THREADS \
+	-vf $FFX_SCALE \
+	-y $FFX_OUTPUT
+}
+ffx-winselect-pa() { 
+	FFX_INFO=$(xwininfo -frame)
+
+	ffmpeg -f $SERVER -ac $FFX_MONO \
+	-i $FFX_PULSE -f x11grab -r $FFX_FPS \
+	-s $(echo $FFX_INFO | grep -oEe 'geometry [0-9]+x[0-9]+'\
+	| grep -oEe '[0-9]+x[0-9]+') \
+	-i :0.0+$(echo $FFX_INFO | grep -oEe 'Corners:\s+\+[0-9]+\+[0-9]+' \
+	| grep -oEe '[0-9]+\+[0-9]+' | sed -e 's/\+/,/' ) \
+	-acodec $FFX_AUDIO -vcodec $FFX_VIDEO \
+       	-preset $FFX_PRESET -crf $FFX_CRF -threads $FFX_THREADS \
+	-vf $FFX_SCALE \
+	-y $FFX_OUTPUT
+}
+
+
+
+$RECORD_FUNC
