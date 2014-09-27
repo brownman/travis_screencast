b437d90d70c04f4d50960a4c499d175aa6ccb3d4
brownman <brownman556@gmail.com>
2014-09-27 12:42:43 +0300

    Update youtube.sh

--- other/youtube.sh
+++ other/youtube.sh
@@ -1,1 +1,20 @@
-#
+sudo apt-get update -qq
+sudo apt-get install -qq recordmydesktop x11-apps libpulse-dev pulseaudio dbus-x11 espeak mbrola-us2 pulseaudio-utils
+eval "export DISPLAY=:99.0"
+eval "sh -e /etc/init.d/xvfb start"
+dbus-launch pulseaudio --start
+eval "recordmydesktop --no-cursor  --output=session.ogv &"
+paplay <(espeak -vmb-us2  "Ok, let's get rickrolled" --stdout)
+eval "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
+sleep 10
+paplay <(espeak -vmb-us2  "That bored me" --stdout)
+killall firefox
+sleep 2
+paplay <(espeak -vmb-us2  "Much better now" --stdout)
+sleep 2
+paplay <(espeak -vmb-us2  "I have to go. See you next build!" --stdout)
+killall recordmydesktop
+while pgrep -x recordmydesktop > /dev/null ; do sleep 1; done # wait for transcoding
+tar cf artifacts.tar session.ogv
+ls -l session.ogv
+mv session.ogv /tmp
