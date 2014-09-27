f09c80a390112b19668f037ea627f62ac814934f
brownman <brownman556@gmail.com>
2014-09-27 16:18:41 +0300

    Update install.sh

--- FFMPEG/install.sh
+++ FFMPEG/install.sh
@@ -8,7 +8,7 @@ tar xzvf ffmpeg-0.6.tar.gz
  #sudo apt-get install xorg-libxfixes
  sudo apt-get install libxfixes-dev
 sudo apt-get install libxext-dev
-./configure --enable-x11grab --enable-gpl --enable-nonfree --extra-cflags="-I/opt/local/include -I/opt/local/include/X11"  --extra-ldflags=-L/opt/local/lib
+commander ./configure --enable-x11grab --enable-gpl --enable-nonfree --extra-cflags="-I/opt/local/include -I/opt/local/include/X11"  --extra-ldflags=-L/opt/local/lib
 
 
 #./configure --prefix="$HOME/ffmpeg_build" \
