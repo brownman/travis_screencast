ea8e88a25c77cdded5b61965ca40625ce545e0f1
brownman <brownman556@gmail.com>
2014-09-29 14:33:53 +0300

    Update install_deps.sh

--- SCRIPTS/install_deps.sh
+++ SCRIPTS/install_deps.sh
@@ -1,19 +1,32 @@
 set -e
 #sudo apt-get update -qq
 #sudo apt-get install -qq recordmydesktop 
+install(){
+  sudo apt-get install $@
+}
+#library deps
+install cowsay libnotify-bin flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
 
 #utils
-sudo apt-get install cowsay libnotify-bin flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
+install imagemagick
+
+#windows control
+install xdotool wmctrl
+
 
 #needed for some tests:
-sudo apt-get install tumiki-fighters
-sudo apt-get install libgtk2.0-0
+install tumiki-fighters
+install libgtk2.0-0
 
 
 #audio
-sudo apt-get install  libpulse-dev pulseaudio espeak mbrola-us2 pulseaudio-utils
+install  libpulse-dev pulseaudio espeak mbrola-us2 pulseaudio-utils
+
+
+#font
+install libgl1-mesa-dri xfonts-cyrillic xfonts-100dpi xfonts-75dpi
 
 #x11
-sudo apt-get installsudo apt-get install imagemagick x11-utils x11-apps dbus-x11 
-sudo apt-get install xdotool wmctrl
+install xvfb \
+x11-utils x11-apps dbus-x11 
 
