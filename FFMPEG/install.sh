3970ebbceeefde35b43d325ef8c0d08f49b23817
brownman <brownman556@gmail.com>
2014-09-29 12:43:21 +0300

    Update install.sh

--- FFMPEG/install.sh
+++ FFMPEG/install.sh
@@ -1,6 +1,14 @@
 #https://github.com/jnvsor/screencap
+#http://www.installion.co.uk/ubuntu/trusty/universe/l/libfrei0r-ocaml-dev/en/install/index.html
 set -e
-sudo apt-get update
+entry1='deb http://us.archive.ubuntu.com/ubuntu trusty main universe'
+#- curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -
+echo "$entry1" | sudo tee -a /etc/apt/sources.list
+  
+  sudo apt-get update
+
+  
+  
 
 sudo apt-get install git build-essential yasm libcdio-paranoia-dev libx264-dev libvpx-dev libvorbis-dev libtheora-dev libspeex-dev libschroedinger-dev librtmp-dev libpulse-dev libopus-dev libopenjpeg-dev libopencv-dev libmp3lame-dev libgsm1-dev libgnutls-dev libfrei0r-ocaml-dev
 git clone https://github.com/FFmpeg/FFmpeg.git --depth=1
