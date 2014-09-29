e059be452a4b1bc2b7767c38fe0f4c1a80c9ba64
brownman <brownman556@gmail.com>
2014-09-29 16:23:55 +0300

    Create install_ffmpeg.sh
    [skip ci]

---
+++ SCRIPTS/install_ffmpeg.sh
@@ -0,0 +1,76 @@
+
+
+pre(){
+sudo apt-get update
+sudo apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev \
+  libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev \
+  libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev
+
+sudo apt-get install yasm
+sudo apt-get install libx264-dev
+sudo apt-get install libopus-dev
+sudo apt-get install libmp3lame-dev
+}
+
+
+manual(){
+mkdir ~/ffmpeg_sources
+#########################################
+sudo apt-get install unzip
+cd ~/ffmpeg_sources
+wget -O fdk-aac.zip https://github.com/mstorsjo/fdk-aac/zipball/master
+unzip fdk-aac.zip
+cd mstorsjo-fdk-aac*
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+make distclean
+#########################################
+
+cd ~/ffmpeg_sources
+wget http://webm.googlecode.com/files/libvpx-v1.3.0.tar.bz2
+tar xjvf libvpx-v1.3.0.tar.bz2
+cd libvpx-v1.3.0
+./configure --prefix="$HOME/ffmpeg_build" --disable-examples
+make
+make install
+make clean
+}
+
+compile(){
+cd ~/ffmpeg_sources
+wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+PATH="$PATH:$HOME/bin" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+  --prefix="$HOME/ffmpeg_build" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" \
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --bindir="$HOME/bin" \
+  --enable-gpl \
+  --enable-libass \
+  --enable-libfdk-aac \
+  --enable-libfreetype \
+  --enable-libmp3lame \
+  --enable-libopus \
+  --enable-libtheora \
+  --enable-libvorbis \
+  --enable-libvpx \
+  --enable-libx264 \
+  --enable-nonfree \
+  --enable-x11grab
+PATH="$PATH:$HOME/bin" make
+make install
+make distclean
+hash -r
+}
+
+
+steps(){
+pre
+manual
+compile
+}
+
+steps
