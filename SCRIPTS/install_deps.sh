6eff10550991db1fc95fdff1c193fa8f5cb63c41
brownman <brownman556@gmail.com>
2014-09-29 15:21:39 +0300

    Update install_deps.sh
    [skip ci]

--- SCRIPTS/install_deps.sh
+++ SCRIPTS/install_deps.sh
@@ -1,21 +1,8 @@
 set -e
 #sudo apt-get update -qq
 #sudo apt-get install -qq recordmydesktop 
-install(){
-  sudo apt-get install $@
-}
-#library deps
-install cowsay libnotify-bin flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
-
-#utils
-install imagemagick
-
-#windows control
-install xdotool wmctrl
 
 
-#needed for some tests:
-install tumiki-fighters
 install libgtk2.0-0
 
 
