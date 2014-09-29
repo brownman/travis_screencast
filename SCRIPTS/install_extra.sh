0c206928b97d72fd21d5f9d92bff438a1c5fda86
brownman <brownman556@gmail.com>
2014-09-29 15:21:26 +0300

    Create install_extra.sh
    [skip ci]

---
+++ SCRIPTS/install_extra.sh
@@ -0,0 +1,12 @@
+#library deps
+install cowsay libnotify-bin flite fortune pv toilet curl wget tree gxmessage zenity xcowsay notify-osd toilet figlet sox libsox-fmt-mp3
+
+#utils
+install imagemagick
+
+#windows control
+install xdotool wmctrl
+
+
+#needed for some tests:
+install tumiki-fighters
