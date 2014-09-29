c8f2869b06bf4254eed06ea19f8706b3e7f36feb
brownman <brownman556@gmail.com>
2014-09-29 14:15:39 +0300

    Update install_deps.sh

--- SCRIPTS/install_deps.sh
+++ SCRIPTS/install_deps.sh
@@ -21,7 +21,7 @@ sudo apt-get install xdotool wmctrl
 #PPA
 sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
 sudo add-apt-repository ppa:fossfreedom/byzanz -y 
-sudo add-apt-repository ppa:jon-severinsson/ffmpeg 
+sudo add-apt-repository ppa:jon-severinsson/ffmpeg -y
 
 #update
 sudo apt-get update 
