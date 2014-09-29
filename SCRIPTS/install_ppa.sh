a711677845de8cc16a0399cbf84b9406dbed254b
brownman <brownman556@gmail.com>
2014-09-29 15:25:26 +0300

    Update install_ppa.sh

--- SCRIPTS/install_ppa.sh
+++ SCRIPTS/install_ppa.sh
@@ -1,10 +1,14 @@
 
-#PPA
+#add PPA
 sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
-sudo add-apt-repository ppa:fossfreedom/byzanz -y 
+#sudo add-apt-repository ppa:fossfreedom/byzanz -y 
 sudo add-apt-repository ppa:jon-severinsson/ffmpeg -y
 
 #update
 sudo apt-get update 
-sudo apt-get install yad byzanz ffmpeg
-#sudo ln -s /usr/bin/avconv /usr/bin/ffmpeg 
+
+
+#install 
+install yad 
+#install byzanz
+install ffmpeg
