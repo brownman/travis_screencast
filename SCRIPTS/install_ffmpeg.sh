08e8554d79687740eacdbca87d3fbe9635df1594
brownman <brownman556@gmail.com>
2014-09-30 15:01:54 +0300

    Update install_ffmpeg.sh

--- SCRIPTS/install_ffmpeg.sh
+++ SCRIPTS/install_ffmpeg.sh
@@ -2,8 +2,8 @@
 #https://gist.github.com/holms/7009218
 
 # Add multimedia source
-echo "deb http://www.deb-multimedia.org wheezy main non-free" >> /etc/apt/sources.list
-echo "deb-src http://www.deb-multimedia.org wheezy main non-free" >> /etc/apt/sources.list
+echo "deb http://www.deb-multimedia.org wheezy main non-free" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://www.deb-multimedia.org wheezy main non-free"  | sudo tee -a /etc/apt/sources.list
 apt-get update
 apt-get install deb-multimedia-keyring # if this aborts, try again
 apt-get update
