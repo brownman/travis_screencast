3a1a5e6cff33854d24b4c3fe3e62224b51d99bcc
brownman <brownman556@gmail.com>
2014-10-01 17:55:24 +0300

    Update travis.sh
    [skip ci]

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 steps_travis(){
   source ./config_travis.cfg
   set_env 
-  before_install &&  install
-  before_script &&   script &&    after_script &&   after_success ||   after_failure
+  before_install &&  install1
+  before_script &&   script1 &&    after_script &&   after_success ||   after_failure
 }
 steps_travis
