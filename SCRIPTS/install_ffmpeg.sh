49e912f92c5ce3f36cb2e3d5aca6638b37d183ef
brownman <brownman556@gmail.com>
2014-10-01 13:19:41 +0300

    Update install_ffmpeg.sh

--- SCRIPTS/install_ffmpeg.sh
+++ SCRIPTS/install_ffmpeg.sh
@@ -1,16 +1,24 @@
 
 #https://gist.github.com/holms/7009218
 #https://help.ubuntu.com/community/CompilingSoftware
+#http://www.zoharbabin.com/build-and-install-ffmpeg-and-x264-on-debian-squeeze-the-dumb-guide/
+
 set -e
-install checkinstall
- #st
-# Add multimedia source
+
+add_sources(){
 echo "deb http://www.deb-multimedia.org wheezy main non-free" | sudo tee -a /etc/apt/sources.list
 echo "deb-src http://www.deb-multimedia.org wheezy main non-free"  | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install deb-multimedia-keyring # if this aborts, try again
-#apt-get update
- 
+sudo apt-get install deb-multimedia-keyring # if this aborts, try again 
+}
+
+
+set_env(){
+ mkdir -p /usr/local/share/ffmpeg 
+}
+
+
+install_sources(){
 # Go to local source directory
 cd /usr/local/src
 sudo apt-get install \
@@ -28,6 +36,9 @@ sudo apt-get install \
   checkinstall
  
 # Install all build dependencies for ffmpeg
+}
+
+install_from_source(){
 sudo apt-get build-dep ffmpeg
  
 # Get the actual ffmpeg source code
@@ -35,7 +46,40 @@ apt-get source ffmpeg
 # Go into the ffmpeg source directory
 cd ffmpeg-*
 # Configure it
-./configure \
+./configure 
+}
+# a fix
+ 
+# Generate the debian package (*.deb)
+#commander checkinstall -D --install=no --pkgname=ffmpeg-full --autodoinst=yes -y
+ 
+# if after this step you'll probably will get an error
+# libavcodec/libx264.c:492: undefined reference to `x264_encoder_open_125'
+# here's a solution:
+mp4_static(){
+# apt-get remove x264
+git clone --depth=1 git://git.videolan.org/x264.git
+cd x264
+./configure --enable-shared
+#./configure --enable-static --enable-shared
+make
+make install
+ldconfig
+#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/src/x264/libx264.a 
+
+}
+
+ 
+
+configure1(){
+ ./configure --enable-gpl \
+--enable-nonfree --enable-libfaac --enable-libgsm --enable-libmp3lame \
+--enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-zlib \
+--enable-postproc --enable-swscale --enable-pthreads --enable-x11grab --enable-libdc1394 --enable-version3 \
+--enable-libopencore-amrnb --enable-libopencore-amrwb
+ }
+ configure2(){
+  ./configure \
 --enable-gpl \
 --enable-nonfree \
 --enable-libfaac \
@@ -54,32 +98,15 @@ cd ffmpeg-*
 --enable-version3 \
 --enable-libopencore-amrnb \
 --enable-libopencore-amrwb
- 
-# a fix
-mkdir -p /usr/local/share/ffmpeg 
- 
-# Generate the debian package (*.deb)
-#commander checkinstall -D --install=no --pkgname=ffmpeg-full --autodoinst=yes -y
- 
-# if after this step you'll probably will get an error
-# libavcodec/libx264.c:492: undefined reference to `x264_encoder_open_125'
-# here's a solution:
-cd ..
-apt-get remove x264
-git clone --depth=1 git://git.videolan.org/x264.git
-cd x264
-./configure --enable-static --enable-shared
-make && make install
-ldconfig
-cd .. && cd ffmpeg*
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/src/x264/libx264.a ./configure --enable-gpl \
---enable-nonfree --enable-libfaac --enable-libgsm --enable-libmp3lame \
---enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-zlib \
---enable-postproc --enable-swscale --enable-pthreads --enable-x11grab --enable-libdc1394 --enable-version3 \
---enable-libopencore-amrnb --enable-libopencore-amrwb
+  
+ }
 
-make clean
+install2(){
+#make clean
+install checkinstall
 commander checkinstall -D --install=no --pkgname=ffmpeg-full --autodoinst=yes -y
- 
 # install the package :)
 commander sudo dpkg -i ffmpeg-full_*-1_amd64.deb
+}
+
+install_from_source
