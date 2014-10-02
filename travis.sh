60bad332ac2a08951c859997a873d4974b6600af
brownman <brownman556@gmail.com>
2014-10-02 11:51:14 +0300

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,8 +1,14 @@
 
+step(){
+  set -e
+  local cmd=$1
+  commander $cmd
+}
+
 steps_for_travis(){
   source ./config_travis.cfg
   set_env1
-  before_install &&  install1
-  before_script &&   script1 &&    after_script &&   after_success ||   after_failure
+  before_install &&  step install1
+  step before_script &&   step script1 &&    step after_script &&   step after_success ||   step after_failure
 }
 steps_for_travis
