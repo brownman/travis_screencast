23f2ade9e6fd391a0306f937465d3d8cf155b224
brownman <brownman556@gmail.com>
2014-10-02 18:02:23 +0300

    control dependecies through 1 .txt file
    [skip ci]

--- SCRIPTS/install_deps.sh
+++ SCRIPTS/install_deps.sh
@@ -1,22 +1,8 @@
 
 
 sudo apt-get update -qq
-#sudo apt-get install -qq
 
-#screencast
-#recordmydesktop espeak mbrola-us2
-
-
-install libgtk2.0-0 xcowsay libnotify-bin imagemagick
-
-
-#audio
-install  libpulse-dev pulseaudio  pulseaudio-utils
-
-
-#font
-install libgl1-mesa-dri xfonts-cyrillic xfonts-100dpi xfonts-75dpi
-
-#x11
-install xvfb x11-utils x11-apps dbus-x11 
-
+while read line;do
+test -n "$line" || break
+commander install $line
+done < ./txt/depend.txt
