19b4e60fed12f9a39774a06032c04b9afa8602c9
brownman <brownman556@gmail.com>
2014-10-03 16:41:24 +0300

    Update x11.sh

--- SCRIPTS/x11.sh
+++ SCRIPTS/x11.sh
@@ -2,3 +2,5 @@
   commander "exec $XVFB $DISPLAY -screen $SCREEN -extension RANDR" &
   #commander  sudo sh -e /etc/init.d/xvfb start
   commander  'dbus-launch pulseaudio --start'
+  print color 33 "DISPLAY $DISPLAY"
+  commander export DISPLAY=99.0
