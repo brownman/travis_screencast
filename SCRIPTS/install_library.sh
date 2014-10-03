163a258ad8c4581fc543ec50f34a0011fd46d0c9
brownman <brownman556@gmail.com>
2014-10-03 10:16:47 +0300

    Update install_library.sh

--- SCRIPTS/install_library.sh
+++ SCRIPTS/install_library.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+ 
+skip(){
+echo 1>&2 skipping: $@
+}
 
+trace0(){
+ echo 1>&2 "[-] $@"
+ }
+
+exiting(){
+echo "[ exiting ] $(caller)"
+exit 1
+}
+
+
+commander1() 
+{ 
+    local args=($@);
+    local cmd="${args[@]}";
+    trace0 ---------------
+    trace0 "[cmd] :: $cmd";
+    eval "$cmd" || exiting
+    res="$?";
+    trace0 res: $res;
+    trace0 ---------------
+    return $res
+}
 
 steps_for_library(){
  commander1 cd $HOME
