54bea68820affe6bdff9b2dbe4fce89f900f3208
brownman <brownman556@gmail.com>
2014-10-03 14:08:45 +0300

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -13,7 +13,7 @@ steps_for_travis(){
   #set_env1
   #before_install &&  step install1
   #step before_script &&   step script1 &&    step after_script &&   step after_success ||   step after_failure
-  source $dir_root/INSTALL/library.sh
+  $dir_root/INSTALL/library.sh
   source /tmp/library.cfg
   print ok
 }
