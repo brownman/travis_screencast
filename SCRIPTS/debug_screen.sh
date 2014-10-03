823a686efdee552c38ec99bb48188c4399153cca
brownman <brownman556@gmail.com>
2014-10-03 15:48:53 +0300

    Update debug_screen.sh

--- SCRIPTS/debug_screen.sh
+++ SCRIPTS/debug_screen.sh
@@ -2,7 +2,7 @@ debug_screen(){
 commander xwininfo -root -tree
 #xcowfortune -t 3 &
 commander xcowsay -t 3  "travis: build number: $TRAVIS_BUILD_NUMBER" &
-commander notify-send "travis: build number" "$TRAVIS_BUILD_NUMBER"
+notify-send "travis: build number" "$TRAVIS_BUILD_NUMBER"
 commander sleep 1 
 commander "import -window root $file_product_cover"
 }
