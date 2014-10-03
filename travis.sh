dfc7d258326668761a7050f08f9963129ce529dd
brownman <brownman556@gmail.com>
2014-10-03 10:22:46 +0300

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,13 +1,15 @@
+export dir_root=$(cd $(dirname $0) && pwd )
+source $dir_root/CFG/config_travis.cfg
+
 
 step(){
   set -e
   local cmd=$1
-  echo  "[Cmd] $cmd"
+  echo  "[STEP] $cmd"
   eval "$cmd"
 }
 
 steps_for_travis(){
-  source ./CFG/config_travis.cfg
   set_env1
   before_install &&  step install1
   step before_script &&   step script1 &&    step after_script &&   step after_success ||   step after_failure
