f7cdefb91d58d62374057734f0721df33e137f20
brownman <brownman556@gmail.com>
2014-10-03 15:50:15 +0300

    Update push_to_github.sh

--- SCRIPTS/push_to_github.sh
+++ SCRIPTS/push_to_github.sh
@@ -35,7 +35,7 @@ commander  git clone --depth=1 --quiet --branch=gh-pages https://${GH_TOKEN}@git
  
 
   git add -f .
- commander git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+ commander git commit -m \"Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages\"
 commander   git push -fq origin gh-pages #> /dev/null
  fi
 
