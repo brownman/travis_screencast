00727b2691144b529fbcfc5ee2f1c246bf990dad
brownman <brownman556@gmail.com>
2014-10-03 13:44:58 +0300

    Update library.sh

--- INSTALL/library.sh
+++ INSTALL/library.sh
@@ -32,25 +32,27 @@ steps_for_library(){
  local url_repo=https://github.com/brownman/LIBRARY
   commander1 git clone  --depth=1 $url_repo --branch=develop
   #git archive --format=tar --remote=$url_repo HEAD | tar xf -
-  local dir2
-  dir2=$HOME/LIBRARY
+  commander1 ./LIBRARY/install.sh
+  #local dir2
+  #dir2=$HOME/LIBRARY
   
   #commander tree $dir2
   
-  commander1 source $dir2/library.cfg
-  commander1 source /tmp/library.cfg
-  
-  
-  echo 1>&2 TESITNG use of the LIBRARY:
-  use print
-  commander1 print ok 
+  #commander1 $dir2/library.cfg
 }
 
-library_test(){
+test2(){
  use pipe_translate
 commander subshell pipe_translate hello 
 }
 
+test1(){
+ commander1 source /tmp/library.cfg
+  echo 1>&2 TESITNG use of the LIBRARY:
+  use print
+  commander1 print ok 
+
+}
 
 steps_for_library
 #library_test
