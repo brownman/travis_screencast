bcb2f5de009f54ffe928a9b5893594318e888e26
brownman <brownman556@gmail.com>
2014-10-04 12:28:32 +0300

    Update extra.sh

--- INSTALL/extra.sh
+++ INSTALL/extra.sh
@@ -1,8 +1,23 @@
 #https://github.com/jnvsor/screencap
 #http://www.installion.co.uk/ubuntu/trusty/universe/l/libfrei0r-ocaml-dev/en/install/index.html
+case1(){
 set -e
 entry1="deb http://us.archive.ubuntu.com/ubuntu $(lsb-release -cs) main universe"
 #- curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -
 echo "$entry1" | sudo tee -a /etc/apt/sources.list
   
   sudo apt-get update
+  }
+  
+  
+  
+  case2(){
+  	sudo wget http://www.medibuntu.org/sources.list.d/`lsb_release -cs`.list --output-document=/etc/apt/sources.list.d/medibuntu.list;
+	sudo apt-get -q update;
+	sudo apt-get --yes -q --allow-unauthenticated install medibuntu-keyring; 
+	sudo apt-get -q update
+	commander sudo apt-get install ffmpeg
+	commander whereis ffmpeg
+	}
+
+case1
