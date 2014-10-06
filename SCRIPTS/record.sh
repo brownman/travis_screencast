4fb0bac67a77e271cf93d31fe73001012bac3a4b
brownman <brownman556@gmail.com>
2014-10-06 09:51:33 +0300

    Update record.sh

--- SCRIPTS/record.sh
+++ SCRIPTS/record.sh
@@ -96,7 +96,7 @@ commander ffmpeg -y -i $FFX_OUTPUT \
 -threads 0 -an -f mp4 \
 $dir_product/session.mp4
 #-loglevel quiet /dev/null \
-#/usr/bin/ffmpeg -y -i inputfile.ogv -sameq -s 1280x720 -aspect 16:9 -r 30000/1001 -b 2M -bt 4M -pass 2 -vcodec libx264 -vpre hq -threads 0 -async 1 -acodec libfaac -ac 2 -ab 160k -ar 48000 -loglevel quiet output.mp4
+commander ffmpeg -y -i $FFX_OUTPUT  -sameq -s 1280x720 -aspect 16:9 -r 30000/1001 -b 2M -bt 4M -pass 2 -vcodec libx264 -vpre hq -threads 0 -async 1 -acodec libfaac -ac 2 -ab 160k -ar 48000 $dir_product/session_converted.mp4
 	
 }
 steps(){
