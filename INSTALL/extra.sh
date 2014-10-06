c84b0d8fc9b0268c4317b47d323b7c854435c92b
brownman <brownman556@gmail.com>
2014-10-06 08:58:35 +0300

    Update extra.sh

--- INSTALL/extra.sh
+++ INSTALL/extra.sh
@@ -2,7 +2,7 @@
 #http://www.installion.co.uk/ubuntu/trusty/universe/l/libfrei0r-ocaml-dev/en/install/index.html
 case1(){
 set -e
-entry1="deb http://us.archive.ubuntu.com/ubuntu $(lsb-release -cs) main universe"
+entry1="deb http://us.archive.ubuntu.com/ubuntu $(lsb_release -cs) main universe"
 #- curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -
 echo "$entry1" | sudo tee -a /etc/apt/sources.list
   
