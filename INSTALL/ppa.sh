a55a4c6b669466f781855af90fe1b972186d7524
brownman <brownman556@gmail.com>
2014-10-06 10:35:37 +0300

    Update ppa.sh

--- INSTALL/ppa.sh
+++ INSTALL/ppa.sh
@@ -1,14 +1,21 @@
 
 #add PPA
-sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
+#sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
 #sudo add-apt-repository ppa:fossfreedom/byzanz -y 
 #sudo add-apt-repository ppa:jon-severinsson/ffmpeg -y
 
 #update
-sudo apt-get update 
+#sudo apt-get update 
 
 
 #install 
-install yad 
+#install yad 
 #install byzanz
 #install ffmpeg
+
+set -u
+
+while read line;do
+test -n "$line" || break
+commander sudo add-apt-repository  $line
+done < <( cat $dir_TXT/ppa.txt | grep -v \# )
