51d7fe5a1d66821b2be44c8692a1dd21b313f738
brownman <brownman556@gmail.com>
2014-10-08 15:40:57 +0300

    Update ppa.sh

--- INSTALL/ppa.sh
+++ INSTALL/ppa.sh
@@ -1,21 +1,3 @@
-
-#add PPA
-#sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
-#sudo add-apt-repository ppa:fossfreedom/byzanz -y 
-#sudo add-apt-repository ppa:jon-severinsson/ffmpeg -y
-
-#update
-#sudo apt-get update 
-
-
-#install 
-#install yad 
-#install byzanz
-#install ffmpeg
-
-set -u
-
-
 use act_on_list
 cmd='sudo add-apt-repository'
 act_on_list $dir_TXT/tests.txt "$cmd""
