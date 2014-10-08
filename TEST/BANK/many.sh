d11f5f193b266ab6eb249664808ce74ca669cef8
brownman <brownman556@gmail.com>
2014-10-08 15:30:26 +0300

    Update many.sh

--- TEST/BANK/many.sh
+++ TEST/BANK/many.sh
@@ -1,39 +1,4 @@
-test_library(){
-  use pipe_translate
- # set -x
-  commander pipe_translate hello
-  
-}
-test_game(){
-  tumiki-fighters -fullscreen &
-  sleep 1
-  while :;do
-  xdotool key ctrl
-  sleep .3
-  done
-  
-}
-test_flite(){
-#alias flite=`which flite`
-str=$(dirname `which flite`)
-export PATH=$PATH:$str  
-flite -t hello -o /tmp/1.wav
-paplay <(cat /tmp/1.wav)
-}
-
-test_prototype(){
-cd /tmp
-git clone https://github.com/brownman/prototypes
-cd prototypes
-./LIB/install.sh
-./BANK/sanity_loop/run.sh
-}
-test_simple_gui(){
-  xcowsay hi
-  notify-send hello world
-  gxmessage hi -timeout 3
-  yad 'hello world' --timeout=3
-}
+ 
 test_firefox(){
 # commander  sudo apt-get install firefox -y
 commander "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
@@ -44,17 +9,4 @@ commander sleep 10
 paplay <(espeak -vmb-us2  "That bored me" --stdout)
 killall firefox  
 }
-test_espeak(){
-paplay <(espeak -vmb-us2  "Ok, let's get rickrolled" --stdout)
-#eval "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
-#( TMOUT=20;   commander $router script  )
-sleep 10
-paplay <(espeak -vmb-us2  "That bored me" --stdout)
-sleep 2
-paplay <(espeak -vmb-us2  "Much better now" --stdout)
-sleep 2
-paplay <(espeak -vmb-us2  "I have to go. See you next build!" --stdout)
-}
-test_cow(){
-  xcowsay -t 10 'hello world'
-}
+tess_firefox
