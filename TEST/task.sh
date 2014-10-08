7197d5ef16379e658a8ae4582b9ca114980dff15
brownman <brownman556@gmail.com>
2014-10-08 15:57:58 +0300

    Update task.sh

--- TEST/task.sh
+++ TEST/task.sh
@@ -6,7 +6,8 @@ print color 33 "DISPLAY: $DISPLAY"
 test -f $file_list_session || exiting
 while read line;do
 test -n "$line" || { print ok empty line; break; }
-  (  commander $line  )&
+  file=dir_TEST=$dir_TEST/$line
+  test -f $file && (  commander $line  )&
   sleep $timeout_task
 done <$dir_TXT/task.txt
 
