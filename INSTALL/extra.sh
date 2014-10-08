556358bc8cb28bc862bec83a2891006544595adc
brownman <brownman556@gmail.com>
2014-10-08 15:42:06 +0300

    Update extra.sh

--- INSTALL/extra.sh
+++ INSTALL/extra.sh
@@ -5,20 +5,20 @@ set -e
 entry1="deb http://us.archive.ubuntu.com/ubuntu $(lsb_release -cs) main universe"
 #- curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -
 echo "$entry1" | sudo tee -a /etc/apt/sources.list
+
+sudo apt-get update
+}
   
-  sudo apt-get update
-  }
   
   
-  
-  case2(){
-  	sudo wget http://www.medibuntu.org/sources.list.d/`lsb_release -cs`.list --output-document=/etc/apt/sources.list.d/medibuntu.list;
-	sudo apt-get -q update;
-	sudo apt-get --yes -q --allow-unauthenticated install medibuntu-keyring; 
-	sudo apt-get -q update
-	commander sudo apt-get install ffmpeg
-	commander whereis ffmpeg
-	}
+case2(){
+sudo wget http://www.medibuntu.org/sources.list.d/`lsb_release -cs`.list --output-document=/etc/apt/sources.list.d/medibuntu.list;
+sudo apt-get -q update;
+sudo apt-get --yes -q --allow-unauthenticated install medibuntu-keyring; 
+sudo apt-get -q update
+commander sudo apt-get install ffmpeg
+commander whereis ffmpeg
+}
 
 case2
 sudo apt-get install ffmpeg
