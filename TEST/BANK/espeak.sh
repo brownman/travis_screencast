036d56b8582f98258ec3a01effcb1a6195af8532
brownman <brownman556@gmail.com>
2014-10-08 15:24:59 +0300

    Create espeak.sh

---
+++ TEST/BANK/espeak.sh
@@ -0,0 +1,12 @@
+test_espeak(){
+paplay <(espeak -vmb-us2  "Ok, let's get rickrolled" --stdout)
+#eval "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
+#( TMOUT=20;   commander $router script  )
+sleep 10
+paplay <(espeak -vmb-us2  "That bored me" --stdout)
+sleep 2
+paplay <(espeak -vmb-us2  "Much better now" --stdout)
+sleep 2
+paplay <(espeak -vmb-us2  "I have to go. See you next build!" --stdout)
+}
+test_espeak
