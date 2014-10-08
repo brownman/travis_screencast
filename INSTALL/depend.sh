7650731f782cb0744197e923d6d9b50301d22b96
brownman <brownman556@gmail.com>
2014-10-08 15:41:11 +0300

    Update depend.sh

--- INSTALL/depend.sh
+++ INSTALL/depend.sh
@@ -1,9 +1,3 @@
-
 set -u
-
-#while read line;do
-#test -n "$line" || break
-#commander sudo apt-get install $line
-#done < <( cat $dir_TXT/depend.txt | grep -v \# )
 use act_on_list
 act_on_list $dir_TXT/depend.txt  'sudo apt-get install'
