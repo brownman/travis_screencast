2e81ca54ba5f031ae0d643f3127e9cbe3edd5a94
brownman <brownman556@gmail.com>
2014-10-09 10:52:00 +0300

    Update push_to_github.sh

--- SCRIPTS/push_to_github.sh
+++ SCRIPTS/push_to_github.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 set -u
 
-
-
-
-upload(){
-
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+gitbook(){
+  commander npm install gitbook -g
+  local fmt='Static Website'
+  local dir_readme="$dir_product"
+  commander "gitbook build $dir_readme --format='$fmt'--output=$dir_product/gitbook"
   
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo -e "Starting to update gh-pages\n"
+}
+clone1(){
+    echo -e "Starting to update gh-pages\n"
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 commander  git clone --depth=1 --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$owner/$repo.git  gh-pages #> /dev/null
-  cd gh-pages
-  
-  #test -d old || { mkdir old; }
- # mv *.* old/
- #git rm -rf build
- 
-  
-  dir_new=build/$TRAVIS_BUILD_NUMBER
-  mkdir -p $dir_new
-  
-  mv  $dir_product $dir_new/media #/* files
-  mv /tmp/log $dir_new/log
-  
-  ### summary
-  ls -lR --sort=size $dir_new > $dir_new/log.txt  #_${TRAVIS_BUILD_NUMBER}.txt
-  #ls -l --sort=size files  > files.txt #_${TRAVIS_BUILD_NUMBER}.txt
-  #ln -l ./web/index.html .
- 
-
-  git add -f .
- git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-git push -fq origin gh-pages #> /dev/null
- fi
-
 }
+
+rm2(){
+test -d old || { mkdir old; }
+mv *.* old/
+git rm -rf build
+}
+migrate1(){
+cd gh-pages
+dir_new=build/$TRAVIS_BUILD_NUMBER
+mkdir -p $dir_new
+#
+mv  $dir_product $dir_new/media #/* files
+mv /tmp/log $dir_new/log
+### summary
+ls -lR --sort=size $dir_new > $dir_new/log.txt  #_${TRAVIS_BUILD_NUMBER}.txt
+}
+push1(){
+git add -f .  
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git push -fq origin gh-pages #> /dev/null
+}
+
 steps(){
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  clone1
+  #rm2
+  migrate1
+  push1
+fi
+}
 #validate_products && 
 #assert file_exist $file_product_cover
 #assert file_has_content $file_product
-upload
+ 
 }
 steps
