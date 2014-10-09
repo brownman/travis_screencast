d86335c8d5f1815b788082871d182ee62882ec70
brownman <brownman556@gmail.com>
2014-10-09 10:28:53 +0300

    Update and rename TEST/task.sh to SCRIPTS/task.sh

--- SCRIPTS/task.sh
+++ SCRIPTS/task.sh
@@ -6,7 +6,7 @@ print color 33 "DISPLAY: $DISPLAY"
 test -f $file_list_session || exiting
 while read line;do
 test -n "$line" || { print ok empty line; break; }
-  file=dir_TEST=$dir_TEST/$line
+  file=dir_TEST=$dir_TEST/BANK/$line
   test -f $file && (  commander $line  )&
   sleep $timeout_task
 done <$dir_TXT/task.txt
