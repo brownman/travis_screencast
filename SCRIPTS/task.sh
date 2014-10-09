1c6165e43a16a333545758e1d56570174397bb3c
brownman <brownman556@gmail.com>
2014-10-09 16:13:31 +0300

    Update task.sh

--- SCRIPTS/task.sh
+++ SCRIPTS/task.sh
@@ -6,18 +6,19 @@ $cmd_trap_err_travis
 print color 33 "DISPLAY: $DISPLAY"
 
 expose_file(){
-  gxmessage -file $file -timeout 3 -title "$filename";
+  local file="$1" 
+  local filename=$(basename $file)
+  gxmessage -file "$file" -timeout 3 -title "$filename";
 }
 
 run(){
-local pid file filename
+local pid file
 
 #test -f $file_list_session || exiting
 while read line;do
 
 test -n "$line" || { print ok empty line; break; }
-  file=dir_TEST=$dir_TEST/BANK/${line}.sh
-  filename=$(basename $file)
+  file="$dir_TEST/BANK/${line}.sh"
   commander "assert file_exist $file"
    (   commander $file )&
   #pid=$!
