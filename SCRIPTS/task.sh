2a358b5af4ae76211bf761382a2de9fbfe88ab3d
brownman <brownman556@gmail.com>
2014-10-09 11:04:51 +0300

    Update task.sh

--- SCRIPTS/task.sh
+++ SCRIPTS/task.sh
@@ -3,12 +3,14 @@
 #eval "export DISPLAY=:99.0"
 print color 33 "DISPLAY: $DISPLAY"
 run(){
-local pid file
+local pid file filename
+
 test -f $file_list_session || exiting
 while read line;do
 test -n "$line" || { print ok empty line; break; }
   file=dir_TEST=$dir_TEST/BANK/$line
-  test -f $file && (  commander $file )&
+  filename=$(basename $file)
+  test -f $file && (  gxmessage -file $file -timeout 3 -title "$filename"; commander $file )&
   pid=$!
   sleep $timeout_task
   kill $pid
