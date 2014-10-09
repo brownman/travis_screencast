028642b2a45d0450d4eb9ef7188efe4e2f64b86f
brownman <brownman556@gmail.com>
2014-10-09 11:46:41 +0300

    Update task.sh

--- SCRIPTS/task.sh
+++ SCRIPTS/task.sh
@@ -1,19 +1,25 @@
 
-#export DISPLAY=:99
-#eval "export DISPLAY=:99.0"
+
+
+set -u
+
+commander $cmd_trap_err_travis
+
 print color 33 "DISPLAY: $DISPLAY"
 run(){
+
 local pid file filename
 
-test -f $file_list_session || exiting
+#test -f $file_list_session || exiting
 while read line;do
+
 test -n "$line" || { print ok empty line; break; }
   file=dir_TEST=$dir_TEST/BANK/$line
   filename=$(basename $file)
-  test -f $file && (  gxmessage -file $file -timeout 3 -title "$filename"; commander $file )&
-  pid=$!
-  sleep $timeout_task
-  kill $pid
+  commander test -f $file && (  gxmessage -file $file -timeout 3 -title "$filename"; commander $file )&
+  #pid=$!
+  commander sleep $timeout_task
+  #kill $pid
 done <$dir_TXT/task.txt
 }
 
