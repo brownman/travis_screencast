06ac6b3bee473e250a4804990668f02362b905b7
brownman <brownman556@gmail.com>
2014-10-09 11:13:13 +0300

    Update push_to_github.sh

--- SCRIPTS/push_to_github.sh
+++ SCRIPTS/push_to_github.sh
@@ -21,6 +21,7 @@ test -d old || { mkdir old; }
 mv *.* old/
 git rm -rf build
 }
+
 migrate1(){
 cd gh-pages
 dir_new=build/$TRAVIS_BUILD_NUMBER
@@ -31,6 +32,7 @@ mv /tmp/log $dir_new/log
 ### summary
 ls -lR --sort=size $dir_new > $dir_new/log.txt  #_${TRAVIS_BUILD_NUMBER}.txt
 }
+
 push1(){
 git add -f .  
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
@@ -48,9 +50,5 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   push1
 fi
 }
-#validate_products && 
-#assert file_exist $file_product_cover
-#assert file_has_content $file_product
- 
-}
+
 steps
