12a09e6ea9a562f4e7767b81b9c6ef3f8bf26488
brownman <brownman556@gmail.com>
2014-10-09 15:38:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ run(){
 local cmd args size
 args="$@"
 cmd="$dir_SCRIPT/${args}.sh"
-echo "[TASK] $cmd"
-eval "$cmd"
+echo "[TASK] $cmd" 
+eval "$cmd" 2>/tmp/log/${filename_init}.err 1>/tmp/log/${filename_init}.out
 }
  ################################## load .cfg + RUN
 #source $dir_root/CFG/config.cfg
 #steps_for_config
 #source $dir_root/CFG/config_ffmpeg.cfg
 
-
+filename_init=$1
 eval "run $filename_init"
