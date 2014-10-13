f5d396079c73d21a7dcd09f31e3cb23da194c69e
brownman <brownman556@gmail.com>
2014-10-13 12:20:00 +0300

    Update shopping.sh

--- wiki/shopping.sh
+++ wiki/shopping.sh
@@ -2,3 +2,4 @@ worldwide free sheeping + paypal
 ------
 - http://www.shoestation.com/saucony-cohesion-5-25118-1-silverblue/pampaacojhdedgkg/product
 - https://www.footwearetc.com/
+- https://www.internationalcheckout.com
