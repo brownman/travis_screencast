8886520f8573e5aa532c950aeba3b201011ce3c9
brownman <brownman556@gmail.com>
2014-10-13 08:38:50 +0300

    Update youtube_upload.sh
    [skip ci]

--- SCRIPTS/youtube_upload.sh
+++ SCRIPTS/youtube_upload.sh
@@ -1,16 +1,6 @@
 
 steps_for_youtube(){
 
-VERSION=0.7.3
-wget http://youtube-upload.googlecode.com/files/youtube-upload-$VERSION.tgz
-tar xvzf youtube-upload-$VERSION.tgz
-cd youtube-upload-$VERSION
-
-sudo apt-get install -y libcurl4-gnutls-dev librtmp-dev
-sudo pip install pycurl gdata progressbar 
-sudo python setup.py install 
-
-
 file_vid=$dir_product/session.mp4
 ls -l $file_vid
 
