2ee9ee9b52702df53016cc23b363ee09e8edb833
brownman <brownman556@gmail.com>
2014-12-28 00:12:08 +0200

    Update firefox.sh

--- TEST/BANK/firefox.sh
+++ TEST/BANK/firefox.sh
@@ -3,7 +3,7 @@ test_firefox(){
 # commander  sudo apt-get install firefox -y
 commander "firefox www.youtube.com/watch?v=oHg5SJYRHA0 &"
 xcowsay "$( wmctrl -l )"
-#wmctrl -a youtube
+wmctrl -a perform
 xdotool key space
 commander sleep 10
 paplay <(espeak -vmb-us2  "That bored me" --stdout)
